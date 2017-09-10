68525eef1b19e414240cba04dc23d14ee5d36b50
Asimov <dev2life@gmail.com>
2017-09-10 14:12:07 +0700

    push

--- bot.php
+++ bot.php
@@ -29,7 +29,7 @@ if (!is_null($events['events'])) {
 			//$text = file_get_contents($url);
 			//if($text =='โอจา อินโฟ') $text=$event['source'];
 			if($text =='โอจา ไอดี') $text=$userId;
-			else if($text =='โอจา ไอดีห้อง') $text=$groupID;
+			//else if($text =='โอจา ไอดีห้อง') $text=$groupID;
 			else $text = '';
 
 			if($text != '')
