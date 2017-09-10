09d58d8afc0dfd12580cc8d69cd10ee608dea62c
Asimov <dev2life@gmail.com>
2017-09-10 11:20:07 +0700

    push-json

--- lib.php
+++ lib.php
@@ -15,7 +15,7 @@
     }
 
 
-    function Push(string $token) {
+    function Push() {
         $access_token = 'mApA9dA4vBkZddHCyLrQ6xkK4FOBQWii2hCpCp2TaH340/LB60kdCjlZFxoxZkAWRudTMqnXefQkEh8v1V92dAFNDbWovSt+vGDpYoUdIzVHmDJfL+XkVrTLDWug46RACDK4NU0UuLvAHav8PlC+ZQdB04t89/1O/w1cDnyilFU=ISSUE';
         echo token . '****';
         // Get POST body content
@@ -25,7 +25,7 @@
 
         // Build message to reply back
         //$url = 'http://democlaimpa.rvp.co.th/Services/line_reply.ashx?text='.urlencode('ดัม');
-        $url = 'http://163.44.197.45/OJAmeeting/LineAPI/Push?token='.$token;
+        $url = 'http://163.44.197.45/OJAmeeting/LineAPI/Push?token='.'9999';
         echo $url;
         $json = file_get_contents($url);
 
