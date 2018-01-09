85c94e602fa8724bd0f6585bd0fcc65040e212c1
Asimov <dev2life@gmail.com>
2018-01-09 23:43:32 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -36,7 +36,7 @@ if (!is_null($events['events'])) {
 			//else if(substr($text,0,40) =='โอจา ลงทะเบียน') {		
 			else if(substr($text,0,5) =='REGIS') {
 				//$url = 'http://163.44.197.45/OJAmeeting/LineAPI/GroupRegister';
-				$url = 'http://ncjadfollowup.oja.go.th/OJAmeeting/LineAPI/GroupRegister';
+				$url = 'http://ncjadfollowup.oja.go.th/LineAPI/GroupRegister';
 				$url .= '?token=9999';
 				$url .= '&lineGroupID='.$groupID;
 				//$url .= '&lineMeetingID='.substr($text,-8);
